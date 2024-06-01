9a9a15161076ad951f8452b3d662324794c3d628
ihiteshpandey <hiteshpandey183@gmail.com>
2024-06-01 15:29:17 +0530

    changes in src folder

---
+++ basic-react/src/App.js
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <h1>Chai aur react | Hitesh Pandey 
+    <br />
+    <br />
+
+    1. Learned to install react . <br />
+    2. Created a repository for learning react .<br />
+    3. Installed basic-react .<br />
+    4. Installed vite-react .<br /> 
+    </h1>
+  );
+}
+
+export default App;
---
+++ vite-react/src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <h1>Chai aur react with vite | Hitesh Pandey 
+    <br />
+    <br />
+
+    1. Learned to install  vite react . <br />
+    2. Created a repository for learning react .<br />
+    3. Installed basic-react .<br />
+
+    </h1>
+  );
+}
+
+export default App
+
